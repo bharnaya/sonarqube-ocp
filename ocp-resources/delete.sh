fe24212523e102ae037cd6eab82e02a2eaca640c
icici-xpress <milind@cateina.com>
2021-01-07 18:49:45 +0530

    Add

--- ocp-resources/delete.sh
+++ ocp-resources/delete.sh
@@ -1,4 +1,4 @@
 oc delete secret sonarqube-admin-password postgresql-sonar-password
-oc delete all -l app=sonarqube
+oc delete all -l app=sonar
 oc delete pvc postgresql-sonarqube-data  sonarqube-data
 
